599a917a9cb1d3d2de5f35f1ed1ccd8c483d4423
dzverkov <dzverkov@gmail.com>
2019-02-27 14:43:31 +0300

    Update task-35.py
    Изменил инициализацию max_negative_num

--- task-35.py
+++ task-35.py
@@ -8,13 +8,16 @@ MAX_LIMIT = 100
 array = [random.randint(MIN_LIMIT, MAX_LIMIT) for _ in range(SIZE)]
 print(array)
 
-max_negative_num = MIN_LIMIT
+max_negative_num = 0
 max_negative_num_idx = 0
 
 for i in range(len(array)):
-    if max_negative_num < array[i] < 0:
-        max_negative_num = array[i]
-        max_negative_num_idx = i
+    if array[i] < 0:
+        if max_negative_num == 0:
+            max_negative_num = array[i]
+        elif max_negative_num < array[i]:
+            max_negative_num = array[i]
+            max_negative_num_idx = i
 
 print(f"Максимальный отрицательный элемент: {max_negative_num}")
 print(f"Позиция максимального отрицательного элемента: {max_negative_num_idx}")
